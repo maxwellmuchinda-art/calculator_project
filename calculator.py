aeb6f1c44a1a53747791d32d188768d3a58db873
maxwellmuchinda-art <maxwellmuchinda@gmail.com>
2025-11-08 16:12:26 +0200

    declaring variables and addition function

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# lab1_calculator.py
+# ICT111 - Lab 1 Python Calculator
+# GROUP 9
+# lab1_calculator.py
+# 
+history = []
+operations_count = 0
+memory = 0.0
+last_result = None
+def format_number(x):
+    if x is None:
+        return "Error"
+    if isinstance(x, float) and x.is_integer():
+        return str(int(x))
+    return str(x)
+def add(a, b):
+    return a + b 
